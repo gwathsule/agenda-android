f7e37b46d7b6c6da84d0b41cdda6dede52265571
rafael dos Santos Gomes <rafael-dos-santos@hotmail.com>
2016-11-27 11:12:59 -0200

    Aula 10 - Agenda de Contatos - Declarando e configurando os objetos da interface

--- ListaContatos/app/src/main/java/com/example/rafael/agenda_contatos/Contatos.java
+++ ListaContatos/app/src/main/java/com/example/rafael/agenda_contatos/Contatos.java
@@ -9,6 +9,8 @@ import android.content.*;
 public class Contatos extends AppCompatActivity implements android.view.View.OnClickListener {
 
     private ImageButton btAdicionar;
+    private EditText txtPesquisa;
+    private ListView lstContatos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -16,12 +18,15 @@ public class Contatos extends AppCompatActivity implements android.view.View.OnC
         setContentView(R.layout.contatos);
 
         btAdicionar = (ImageButton) findViewById(R.id.btAdicionar);
+        txtPesquisa = (EditText) findViewById(R.id.txtPesquisa);
+        lstContatos = (ListView) findViewById(R.id.lstContatos);
+
         btAdicionar.setOnClickListener(this);//parei aos 43:32
     }
 
     @Override
     public void onClick(View v) {
-        Intent it = new Intent(this, NovoCliente.class);
+        Intent it = new Intent(this, NovoContato.class);
         startActivity(it);
     }
 }
---
+++ ListaContatos/app/src/main/java/com/example/rafael/agenda_contatos/NovoContato.java
@@ -0,0 +1,83 @@
+package com.example.rafael.agenda_contatos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+public class NovoContato extends AppCompatActivity {
+
+    private EditText txtNome;
+    private EditText txtTelefone;
+    private EditText txtEmail;
+    private EditText txtEndereço;
+    private EditText txtDatasEspeciais;
+    private EditText txtGrupo;
+
+    private Spinner spnTipoTelefone;
+    private Spinner spnTipoEmail;
+    private Spinner spnTipoEndereco;
+    private Spinner spnTipoDatasEspeciais;
+
+    private ArrayAdapter<String> adpTipoTelefone;
+    private ArrayAdapter<String> adpTipoEmail;
+    private ArrayAdapter<String> adpTipoEndereco;
+    private ArrayAdapter<String> adpTipoDatasEspeciais;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.novo_contato);
+
+        txtNome = (EditText) findViewById(R.id.txtNome);
+        txtTelefone = (EditText) findViewById(R.id.txtTelefone);
+        txtEmail = (EditText) findViewById(R.id.txtEmail);
+        txtEndereço = (EditText) findViewById(R.id.txtEndereco);
+        txtDatasEspeciais = (EditText) findViewById(R.id.txtDatasEspeciais);
+        txtGrupo = (EditText) findViewById(R.id.txtGrupo);
+
+        spnTipoTelefone = (Spinner) findViewById(R.id.spnTipoTelefone);
+        spnTipoEmail = (Spinner) findViewById(R.id.spnTipoEmail);
+        spnTipoEndereco = (Spinner) findViewById(R.id.spnTipoEndereco);
+        spnTipoDatasEspeciais= (Spinner) findViewById(R.id.spnTipoDatasEspeciais);
+
+        //construtor dos spinner passando como referencia essa activity e setando o layout padrão "simple_spinner_item"
+        adpTipoTelefone = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
+        adpTipoTelefone.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        adpTipoEmail = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
+        adpTipoEmail.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        adpTipoEndereco = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
+        adpTipoEndereco.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        adpTipoDatasEspeciais = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
+        adpTipoDatasEspeciais.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        //associa cada objeto do tipo spinner à um objet do tipo array adapter
+        spnTipoTelefone.setAdapter(adpTipoTelefone);
+        spnTipoEmail.setAdapter(adpTipoEmail);
+        spnTipoEndereco.setAdapter(adpTipoEndereco);
+        spnTipoDatasEspeciais.setAdapter(adpTipoDatasEspeciais);
+
+        //adiciona itens no menu do spinner para cada spinner;
+        adpTipoTelefone.add("Celular");
+        adpTipoTelefone.add("Casa");
+        adpTipoTelefone.add("Trabalho");
+        adpTipoTelefone.add("Outros");
+
+        adpTipoEmail.add("Pessoal");
+        adpTipoEmail.add("Trabalho");
+        adpTipoEmail.add("Outros");
+
+        adpTipoEndereco.add("Casa");
+        adpTipoEndereco.add("Trabalho");
+        adpTipoEndereco.add("Outros");
+
+        adpTipoDatasEspeciais.add("Aniversário");
+        adpTipoDatasEspeciais.add("Morte");
+        adpTipoDatasEspeciais.add("Outros");
+    }
+}
